6a7ea14a0f35aca6f05486422feefcfb3d9349f3
TonyLin <qjwdcool@qq.com>
2015-06-16 17:30:44 +0800

    1.修改版本1.1.1

--- GITestDemo/Base/BaseViewController.m
+++ GITestDemo/Base/BaseViewController.m
@@ -68,6 +68,7 @@
 
 - (void)initBLESDK{
     
+    NSString *shangHuName  = [[NSUserDefaults standardUserDefaults] objectForKey:kShangHuName];
     NSString *shangHu = [[NSUserDefaults standardUserDefaults] stringForKey:kShangHuEditor];
     NSString *zhongDuan = [[NSUserDefaults standardUserDefaults] stringForKey:kZhongDuanEditor];
     NSString *caoZhuoYuan = [[NSUserDefaults standardUserDefaults] stringForKey:kCaoZhuoYuanEditor];
@@ -85,14 +86,16 @@
         caoZhuoYuan = @"01";
     }
     if (!host) {
-        host = @"122.112.12.20";
-        [[NSUserDefaults standardUserDefaults] setObject:host forKey:kHostEditor];
+        //host = @"122.112.12.25";
+        [[NSUserDefaults standardUserDefaults] setObject:kServerIP forKey:kHostEditor];
     }
     if (!port) {
-        port = @"6889";
-        [[NSUserDefaults standardUserDefaults] setObject:port forKey:kPortEditor];
+        //port = @"18081";
+        [[NSUserDefaults standardUserDefaults] setObject:kServerPort forKey:kPortEditor];
     }
-    
+    if (!shangHuName) {
+        [[NSUserDefaults standardUserDefaults] setObject:@"周黑鸭" forKey:kShangHuName];
+    }
     [[NSUserDefaults standardUserDefaults]synchronize];
     
     MiniPosSDKInit();
