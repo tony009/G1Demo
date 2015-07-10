8f8f3fef2dff1c3266db9bc0f8bb49a27723170f
TonyLin <qjwdcool@qq.com>
2015-07-10 14:13:39 +0800

    1.修复通用界面 输入联系方式时键盘遮挡界面的问题

--- GITestDemo/CommonViewController.h
+++ GITestDemo/CommonViewController.h
@@ -10,5 +10,5 @@
 
 @interface CommonViewController : UIViewController
 @property (strong, nonatomic) IBOutlet UILabel *phoneNo;
-
+@property (strong, nonatomic) IBOutlet UITextField *phoneNoTextField;
 @end
